42ce0f20911cd045582c7aa803595c20820f12ac
Paolo R <paolorizla31@gmail.com>
2026-05-29 22:32:31 +0200

    Aggiunto bozza script installazione da completare

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/bin/bash
 
 # Interromopi lo script al primo errore
 set -e
@@ -12,29 +12,29 @@ SOURCE_DIRS=("$CONFIG_FILES" "$SERVICE_FILES" "$BIN_FILES")
 
 # Funzione di rollback
 cleanup() {
-	local exit_code=$?
+  local exit_code=$?
 
-	# Se non ci sono errori non fare nulla
-	if [ $exit_code -eq 0 ]; then
-		return
-	fi
-	echo ""
-	echo "!!! ERRORE RILEVATO, ESECUZIONE ROLLBACK"
+  # Se non ci sono errori non fare nulla
+  if [ $exit_code -eq 0 ]; then
+    return
+  fi
+  echo ""
+  echo "!!! ERRORE RILEVATO, ESECUZIONE ROLLBACK"
 
-	# Eseguii rollback su errore
-	for dir in "${SOURCE_DIRS[@]}/*"; do
-		for file in $dir; do
-			local dest_file="/$dir/$file"
-			if [ -f "$dedst_file" ]; then
-				rm -f "$dest_file"
-				echo "Rimosso file $dest_file"
-			fi
-		done
-	done
+  # Eseguii rollback su errore
+  for dir in "${SOURCE_DIRS[@]}/*"; do
+    for file in $dir; do
+      local dest_file="/$dir/$file"
+      if [ -f "$dest_file" ]; then
+        rm -f "$dest_file"
+        echo "Rimosso file $dest_file"
+      fi
+    done
+  done
 
-	# Nota: systemctl daemon-reload non è reversibile, ma lo script si blocca prima se cp fallisce.
-	
-	echo "Rollback completato. Installazione annullata"
+  # Nota: systemctl daemon-reload non è reversibile, ma lo script si blocca prima se cp fallisce.
+
+  echo "Rollback completato. Installazione annullata"
 }
 
 # Registra la funzione cleanup all'uscita
@@ -42,11 +42,10 @@ trap cleanup EXIT
 
 # Verifica privilegio di root
 if [ "$EUIID" -ne 0 ]; then
-	echo "Errore: Eseguire con i privilegi di root"
-	exit 1
+  echo "Errore: Eseguire con i privilegi di root"
+  exit 1
 fi
 
 echo "Avvio installazione..."
 
 # 1. Copia dei files
-
